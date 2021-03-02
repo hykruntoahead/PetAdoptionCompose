0d7e4b0eba44e47d841ce4fafecd6740bca5f672
ykhe <ykhe@grandstream.cn>
2021-03-02 17:18:43 +0800

    run ./gradlew :app:spotlessApply to modify some code

--- app/src/main/java/com/example/androiddevchallenge/homepage/PetAdoptionCards.kt
+++ app/src/main/java/com/example/androiddevchallenge/homepage/PetAdoptionCards.kt
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2021 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.example.androiddevchallenge.homepage
 
 import androidx.compose.foundation.layout.Column
@@ -44,13 +59,15 @@ fun PetImage(pet: Adoption, modifier: Modifier = Modifier) {
 fun AdoptionCard(adoption: Adoption) {
     Row(
         Modifier
-            .padding(8.dp)) {
+            .padding(8.dp)
+    ) {
         PetImage(
             pet = adoption,
             modifier = Modifier
                 .width(100.dp)
                 .height(80.dp)
-                .padding(end = 16.dp))
+                .padding(end = 16.dp)
+        )
 
         Column(Modifier.weight(1f)) {
             PetTitle(pet = adoption)
